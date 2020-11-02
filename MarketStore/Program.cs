38e0d4b3a7c0f01b7dd8e6bacb8fa11775fcf9f7
F <filip.prvulj@gmail.com>
2020-11-02 18:45:40 +0100

    Add: Print output

--- MarketStore/Program.cs
+++ MarketStore/Program.cs
@@ -6,10 +6,36 @@ using System.Threading.Tasks;
 
 namespace MarketStore
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            try
+            {
+                BronzeCard bronzeCard = new BronzeCard(0, 150);
+                SilverCard silverCard = new SilverCard(600, 850);
+                GoldCard goldCard = new GoldCard(1300, 1300);
+
+                Console.WriteLine("1. Bronze");
+                bronzeCard.PrintOutput();
+
+                Console.WriteLine("2. Silver");
+                silverCard.PrintOutput();
+
+                Console.WriteLine("3. Gold");
+                goldCard.PrintOutput();
+            }
+            catch (ArgumentException ae)
+            {
+                Console.WriteLine($"{ae.GetType().Name}: {ae.Message}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadLine();
         }
     }
 }
