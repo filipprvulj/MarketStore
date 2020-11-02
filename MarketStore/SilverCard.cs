3a83ee78da4e2ca188f04a0c41c1d943f7be7cc0
F <filip.prvulj@gmail.com>
2020-11-02 18:43:55 +0100

    Add: Silver Card

---
+++ MarketStore/SilverCard.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketStore
+{
+    internal class SilverCard : DiscountCard
+    {
+        public SilverCard(float turnover, float purchaseValue) : base(turnover, purchaseValue)
+        {
+        }
+
+        public override float DiscountRate { get; set; } = 2.0f;
+
+        public override float CalculateDiscountRate()
+        {
+            if (Turnover > 300)
+            {
+                DiscountRate = 3.5f;
+            }
+
+            return DiscountRate;
+        }
+    }
+}
