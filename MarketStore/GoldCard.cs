ecbb36f8648b8fe5874c5c9ebfddb283a56be223
F <filip.prvulj@gmail.com>
2020-11-02 18:44:20 +0100

    Add: Gold Card

---
+++ MarketStore/GoldCard.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketStore
+{
+    internal class GoldCard : DiscountCard
+    {
+        public GoldCard(float turnover, float purchaseValue) : base(turnover, purchaseValue)
+        {
+        }
+
+        public override float DiscountRate { get; set; } = 2.0f;
+
+        public override float CalculateDiscountRate()
+        {
+            if (Turnover >= 100)
+            {
+                DiscountRate += Turnover / 100;
+
+                if (DiscountRate > 10.0f)
+                {
+                    DiscountRate = 10.0f;
+                }
+            }
+
+            return DiscountRate;
+        }
+    }
+}
